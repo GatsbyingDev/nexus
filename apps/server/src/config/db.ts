40836ccde9a36c738b39ec6169bdcd00212f2196
GatsbyingDev <peternguyentbs16@example.com>
2026-04-05 16:02:50 +0700

    cleanup legacy server files breaking Render TypeScript build

--- apps/server/src/config/db.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./env.js";
-
-export const connectDB = async (): Promise<void> => {
-  await mongoose.connect(env.MONGODB_URI);
-};
--- apps/server/src/routes/invites.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createInvite, getInvite } from "../controllers/invites.controller.js";
-import { requireAuth } from "../middleware/auth.js";
-
-export const invitesRouter = Router();
-
-invitesRouter.post("/servers/:serverId/invites", requireAuth, createInvite);
-invitesRouter.get("/invites/:inviteCode", getInvite);
